e64c13e02aa9615180abaedcea84a4e20e623b36
Samuel Giles <livelikeasam@googlemail.com>
2013-05-23 08:08:33 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samuelgil::Application.config.secret_token = 'd9d6d6b695445e67a6eda3d7635249ec328e6f8a845a4bc4e50c119f752ca078d48dc10773d8ee55a295294d537959a1c58e6c076f4ecf21d0e3afc242f182d7'
